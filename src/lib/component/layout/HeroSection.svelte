1661b222af1e11b9bbe6fc3fddc6deccc9c5b687
Xettri Aleen <54938235+xettrialeen@users.noreply.github.com>
2024-02-22 17:59:04 +0545

    updates

--- src/lib/component/layout/HeroSection.svelte
+++ src/lib/component/layout/HeroSection.svelte
@@ -1,7 +1,110 @@
 <script>
+	import { tweened } from 'svelte/motion';
 	import aleen from '../../../assets/my/aleen.png';
-	import { onMount } from 'svelte';
-	
+	import user1 from '../../../assets/portfilio/user1.png';
+	import user2 from '../../../assets/portfilio/user2.png';
+	import { onMount, onDestroy } from 'svelte';
+	import gsap from 'gsap';
+	let displayedText = '';
+	const fullText =
+		"“Hello, you're welcome to engage in a conversation with me. I'm not an automated bot, however. I'm adept at following commands. Click to initiate a chat.”";
+
+	const words = fullText.split(/\s+/); // Split the text into words
+	let index = 0;
+	let isChat = true;
+	onMount(() => {
+		const interval = setInterval(() => {
+			if (index < words.length) {
+				displayedText += (index > 0 ? ' ' : '') + words[index];
+				index++;
+			} else {
+				clearInterval(interval);
+				isChat = false;
+			}
+		}, 50); // Adjust typing speed by changing the interval time
+	});
+	// let words = text.split(' ');
+	//   let typedText = readable("", (set) => {
+	//     let index = 0;
+	//     const interval = setInterval(() => {
+	//       if (index < words.length) {
+	//         set(words.slice(0, index + 1).join(' '));
+	//         index++;
+	//       } else {
+	//         clearInterval(interval);
+	//       }
+	//     }, 100); // Adjust typing speed here (milliseconds per word)
+
+	//     // Cleanup function
+	//     return () => clearInterval(interval);
+	//   });
+
+	let pointerEventsChatBar = true;
+	function handleChatOpener() {
+		const tl = gsap.timeline();
+		console.log(tl);
+		tl.fromTo(
+			['.chat-animate-container', '.chat-animate-container2'],
+			{
+				scaleX: 1,
+				scaleY: 0
+			},
+			{
+				scaleX: 1,
+				scaleY: 1,
+				ease: 'power2.out', // Use a smoother easing function
+				duration: 0.6, // Increase the duration for a slower transition
+				stagger: {
+					amount: 0.15,
+					ease: 'power2.out'
+				},
+				onComplete: function () {
+					pointerEventsChatBar = false;
+				}
+			},
+			0
+		)
+			.fromTo(
+				['.social-wrapper a'],
+				{
+					y: 0,
+					opacity: 1
+				},
+				{
+					y: 200,
+					opacity: 0,
+					duration: 0.6,
+					delay:0.3,
+					ease: 'sine.out',
+					stagger: {
+						amount: 0.3,
+
+						ease: 'sine.out'
+					}
+				},
+				0
+			)
+			.fromTo(
+				['.chat-box-container','.chat-footer'],
+				{
+					visibility:"hidden",
+					opacity: 0
+				},
+				{
+					visibility:"visible",
+					opacity: 1,
+					duration: 0.6,
+					delay:0.6,
+					ease: 'sine.out',
+					stagger: {
+						amount: 0.3,
+
+						ease: 'sine.out'
+					}
+				},
+				0
+			);
+	}
 </script>
 
 <section class="Hero-Section main-width-center">
@@ -72,15 +175,20 @@
 					fill="none"
 					viewBox="0 0 484 480"
 				>
-				<path class="body" fill="#fff" d="M460.399 456.924H22.7c19.151-100.207 80.434-179.553 158.887-206.75l16.642-24.689c1.585-3.828 9.113-10.561 13.339-10.429l59.17.132c4.095-.265 11.491 1.716 13.076 5.677l17.698 29.309c78.321 27.197 139.604 106.543 158.887 206.75Z">
-					<animateTransform
-					attributeName="transform"
-					attributeType="XML"
-					type="translate"
-					dur="3s"
-					values="0,0; 0,2; 0,0"
-					repeatCount="indefinite"/>
-				</path>
+					<path
+						class="body"
+						fill="#fff"
+						d="M460.399 456.924H22.7c19.151-100.207 80.434-179.553 158.887-206.75l16.642-24.689c1.585-3.828 9.113-10.561 13.339-10.429l59.17.132c4.095-.265 11.491 1.716 13.076 5.677l17.698 29.309c78.321 27.197 139.604 106.543 158.887 206.75Z"
+					>
+						<animateTransform
+							attributeName="transform"
+							attributeType="XML"
+							type="translate"
+							dur="3s"
+							values="0,0; 0,2; 0,0"
+							repeatCount="indefinite"
+						/>
+					</path>
 					<path
 						fill="#fff"
 						fill-opacity=".76"
@@ -108,7 +216,7 @@
 						d="M460.399 456.924H22.7c19.151-100.207 80.434-179.553 158.887-206.75l18.359-27.197c3.698-6.601 11.358-8.318 15.453-8.186h49.792c4.227-.132 14.661 0 18.623 6.074l17.698 29.309c78.321 27.197 139.604 106.543 158.887 206.75Z"
 					/>
 					<path
-					class="neck"
+						class="neck"
 						fill="#fff"
 						stroke="#000"
 						stroke-width="1.556"
@@ -144,20 +252,81 @@
 						stroke-width="1.037"
 						d="M299.135 159.209c0-5.413 4.358-9.901 9.905-9.901"
 					/>
-					<path class="face" fill="#fff" stroke="#000" stroke-width="1.556" d="M295.304 164.754c-.924 26.801-23.509 47.925-50.321 47.133l-15.717-.528c-26.811-.792-47.943-23.5-47.151-50.169l1.717-52.546c.793-26.669 23.51-47.925 50.189-47l15.849.528c26.812.792 47.944 23.368 47.019 50.169l-1.585 52.413Z">
-						<animate attributeName="transform" attributeType="XML" type="translate" values="0,0; 0,2; 0,0" dur="1s" repeatCount="indefinite"/>
-					</path>
-				
-					<animate
-        attributeName="d"
-        dur="0.5s"
-        values="M255,170 Q235,180 215,170; M255,170 Q235,160 215,170; M255,170 Q235,180 215,170"
-        repeatCount="indefinite"/>
-					<path class="mouth" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.037" d="M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z">
-						<animate attributeName="d" values="M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z; M255.021 174.507c-1.188 2.13-7.396 7.542-14.792 7.278-7.529-.264-13.34-3.073-14-10.202l28.792.924Z; M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z" dur="1s" repeatCount="indefinite" />
-					</path>
 					<path
-						fill="#000" class="eyes"
+						class="face"
+						fill="#fff"
+						stroke="#000"
+						stroke-width="1.556"
+						d="M295.304 164.754c-.924 26.801-23.509 47.925-50.321 47.133l-15.717-.528c-26.811-.792-47.943-23.5-47.151-50.169l1.717-52.546c.793-26.669 23.51-47.925 50.189-47l15.849.528c26.812.792 47.944 23.368 47.019 50.169l-1.585 52.413Z"
+					>
+						<animate
+							attributeName="transform"
+							attributeType="XML"
+							type="translate"
+							values="0,0; 0,2; 0,0"
+							dur="1s"
+							repeatCount="indefinite"
+						/>
+					</path>
+
+					<animate
+						attributeName="d"
+						dur="0.5s"
+						values="M255,170 Q235,180 215,170; M255,170 Q235,160 215,170; M255,170 Q235,180 215,170"
+						repeatCount="indefinite"
+					/>
+
+					{#if isChat}
+						<path
+							class="mouth"
+							stroke="#000"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.037"
+							d="M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z"
+						>
+							<animate
+								attributeName="d"
+								values="M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z; M255.021 174.507c-1.188 2.13-7.396 7.542-14.792 7.278-7.529-.264-13.34-3.073-14-10.202l28.792.924Z; M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z"
+								dur="1s"
+								repeatCount="indefinite"
+							/>
+						</path>
+
+						<path
+							class="hair"
+							fill="#000"
+							d="m310.757 138.614-12.415 7.525-1.585-17.427c-16.113-6.337-15.321-28.385-15.321-28.385-3.698 10.958 1.717 25.48 1.717 25.48-24.83-9.241-20.339-39.211-20.471-37.89-2.774 18.351-22.717 26.536-41.736 30.101-1.453.264 6.075-3.697 12.151-9.506 7.264-7.129 13.736-16.37 13.736-16.37s-12.416 9.241-16.642 12.277c-15.717 10.958-25.623 10.958-25.623 10.958s13.868-12.146 22.849-19.935c-17.962 13.994-42.528 25.216-42.528 25.216l-3.17 18.22-19.547-1.057s-20.34-40.399-5.019-77.498c15.189-37.23 66.038-39.739 83.472-36.703 14.396 2.509 16.377 5.678 17.566 8.714.528 1.32 1.453-3.565 15.981-2.904 23.642 1.056 48.34 24.16 51.906 52.941 4.887 38.815-15.321 56.243-15.321 56.243Z"
+						>
+							<animateTransform
+								attributeName="transform"
+								attributeType="XML"
+								type="translate"
+								dur="3s"
+								values="0,0; 0,-2; 0,0"
+								repeatCount="indefinite"
+							/>
+						</path>
+					{:else}
+						<path
+							class="mouth"
+							stroke="#000"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.037"
+							d="M255.021 169.507c-1.188 7.13-7.396 12.542-14.792 12.278-7.529-.264-13.34-6.073-14-13.202l28.792.924Z"
+						/>
+
+						<path
+							class="hair"
+							fill="#000"
+							d="m310.757 138.614-12.415 7.525-1.585-17.427c-16.113-6.337-15.321-28.385-15.321-28.385-3.698 10.958 1.717 25.48 1.717 25.48-24.83-9.241-20.339-39.211-20.471-37.89-2.774 18.351-22.717 26.536-41.736 30.101-1.453.264 6.075-3.697 12.151-9.506 7.264-7.129 13.736-16.37 13.736-16.37s-12.416 9.241-16.642 12.277c-15.717 10.958-25.623 10.958-25.623 10.958s13.868-12.146 22.849-19.935c-17.962 13.994-42.528 25.216-42.528 25.216l-3.17 18.22-19.547-1.057s-20.34-40.399-5.019-77.498c15.189-37.23 66.038-39.739 83.472-36.703 14.396 2.509 16.377 5.678 17.566 8.714.528 1.32 1.453-3.565 15.981-2.904 23.642 1.056 48.34 24.16 51.906 52.941 4.887 38.815-15.321 56.243-15.321 56.243Z"
+						/>
+					{/if}
+
+					<path
+						fill="#000"
+						class="eyes"
 						d="M209.455 134.653c-.264 3.564-3.302 6.337-7 6.073-3.566-.264-6.34-3.301-6.076-6.997.265-3.565 3.302-6.338 7-6.073 3.567.264 6.34 3.3 6.076 6.997Zm69.34 1.452c-.132 3.697-3.302 6.469-6.868 6.205-3.698-.264-6.472-3.432-6.208-6.997.264-3.565 3.434-6.337 7-6.073 3.566.132 6.34 3.3 6.076 6.865Z"
 					/>
 					<path
@@ -167,15 +336,7 @@
 						stroke-width="1.556"
 						d="M266.115 121.318s9.246-1.848 16.114 3.433m-72.51-4.621s-9.245-2.376-16.377 2.641m51.641 28.385-11.887-.264 6.208-25.745"
 					/>
-					<path class="hair" fill="#000" d="m310.757 138.614-12.415 7.525-1.585-17.427c-16.113-6.337-15.321-28.385-15.321-28.385-3.698 10.958 1.717 25.48 1.717 25.48-24.83-9.241-20.339-39.211-20.471-37.89-2.774 18.351-22.717 26.536-41.736 30.101-1.453.264 6.075-3.697 12.151-9.506 7.264-7.129 13.736-16.37 13.736-16.37s-12.416 9.241-16.642 12.277c-15.717 10.958-25.623 10.958-25.623 10.958s13.868-12.146 22.849-19.935c-17.962 13.994-42.528 25.216-42.528 25.216l-3.17 18.22-19.547-1.057s-20.34-40.399-5.019-77.498c15.189-37.23 66.038-39.739 83.472-36.703 14.396 2.509 16.377 5.678 17.566 8.714.528 1.32 1.453-3.565 15.981-2.904 23.642 1.056 48.34 24.16 51.906 52.941 4.887 38.815-15.321 56.243-15.321 56.243Z">
-						<animateTransform
-						attributeName="transform"
-						attributeType="XML"
-						type="translate"
-						dur="3s"
-						values="0,0; 0,-2; 0,0"
-						repeatCount="indefinite"/>
-					</path>
+
 					<path
 						stroke="#000"
 						stroke-linecap="round"
@@ -206,8 +367,95 @@
 						d="M278.927 228.918c-7.264 6.865-20.472 11.354-35.66 11.354-16.642 0-31.038-5.545-37.642-13.466m84.132 28.649-15.189 24.16-31.302-24.16-24.566 24.16-23.905-23.632"
 					/>
 				</svg>
+				<!-- <p>
+					Today is a gift that’s why it is called present
+				</p> -->
 
-				<p>“Today is a gift that’s why it is called present”</p>
+				{#if !isChat}
+					<button class="chat-opener-action" on:click={handleChatOpener}
+						><span> Click Here </span>
+
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="22"
+							height="24"
+							fill="none"
+							viewBox="0 0 22 24"
+						>
+							<g clip-path="url(#a)">
+								<path
+									fill="#fff"
+									d="M5.284 17.596c-.359-.458-.801-1.39-1.58-2.55-.443-.634-1.535-1.84-1.87-2.466a1.604 1.604 0 0 1-.191-1.275 1.68 1.68 0 0 1 1.824-1.275 3.524 3.524 0 0 1 1.718.917c.328.313.626.648.9 1.007.207.26.253.359.482.65.229.297.381.587.267.152-.084-.634-.237-1.703-.458-2.657-.16-.725-.199-.84-.351-1.39-.153-.549-.245-1.007-.413-1.625a17.038 17.038 0 0 1-.35-1.863 3.588 3.588 0 0 1 .327-2.351 1.51 1.51 0 0 1 1.657-.283c.562.415.98.994 1.199 1.657.328.817.55 1.672.656 2.542.206 1.275.603 3.13.61 3.512 0-.466-.083-1.458 0-1.909.092-.458.413-.84.856-1.015a2.525 2.525 0 0 1 1.168-.077c.397.077.748.306.985.634.29.748.458 1.527.48 2.328a8.254 8.254 0 0 1 .36-2.06 1.59 1.59 0 0 1 .877-.611c.42-.077.848-.077 1.267 0 .352.114.65.335.87.626a7.16 7.16 0 0 1 .482 2.114c0 .176.091-.496.366-.946a1.28 1.28 0 0 1 2.05-.554 1.277 1.277 0 0 1 .37 1.37v2.878c-.046.74-.153 1.481-.305 2.214a7.52 7.52 0 0 1-.916 1.817 9.253 9.253 0 0 0-1.512 2.306 5.093 5.093 0 0 0 .023 2.442 7.663 7.663 0 0 1-1.565 0c-.496-.076-1.107-1.068-1.275-1.374a.48.48 0 0 0-.862 0c-.283.489-.901 1.367-1.336 1.413-.855.106-2.611 0-3.993 0 0 0 .229-1.268-.298-1.733-.519-.458-1.053-.993-1.45-1.344l-1.069-1.221Z"
+								/>
+								<path
+									stroke="#11031C"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="1.718"
+									d="M5.284 17.596c-.359-.458-.801-1.39-1.58-2.55-.443-.634-1.535-1.84-1.87-2.466a1.604 1.604 0 0 1-.191-1.275 1.68 1.68 0 0 1 1.824-1.275 3.524 3.524 0 0 1 1.718.917c.328.313.626.648.9 1.007.207.26.253.359.482.65.229.297.381.587.267.152-.084-.634-.237-1.703-.458-2.657-.16-.725-.199-.84-.351-1.39-.153-.549-.245-1.007-.413-1.625a17.038 17.038 0 0 1-.35-1.863 3.588 3.588 0 0 1 .327-2.351 1.51 1.51 0 0 1 1.657-.283c.562.415.98.994 1.199 1.657.328.817.55 1.672.656 2.542.206 1.275.603 3.13.61 3.512 0-.466-.083-1.458 0-1.909.092-.458.413-.84.856-1.015a2.525 2.525 0 0 1 1.168-.077c.397.077.748.306.985.634.29.748.458 1.527.48 2.328a8.254 8.254 0 0 1 .36-2.06 1.59 1.59 0 0 1 .877-.611c.42-.077.848-.077 1.267 0 .352.114.65.335.87.626a7.16 7.16 0 0 1 .482 2.114c0 .176.091-.496.366-.946a1.28 1.28 0 0 1 2.05-.554 1.277 1.277 0 0 1 .37 1.37v2.878c-.046.74-.153 1.481-.305 2.214a7.52 7.52 0 0 1-.916 1.817 9.253 9.253 0 0 0-1.512 2.306 5.093 5.093 0 0 0 .023 2.442 7.663 7.663 0 0 1-1.565 0c-.496-.076-1.107-1.068-1.275-1.374a.48.48 0 0 0-.862 0c-.283.489-.901 1.367-1.336 1.413-.855.106-2.611 0-3.993 0 0 0 .229-1.268-.298-1.733-.519-.458-1.053-.993-1.45-1.344l-1.069-1.221Z"
+								/>
+								<path
+									fill="#11031C"
+									d="M15.827 17.954v-4.763c0-.29-.237-.52-.535-.52a.52.52 0 0 0-.519.52v4.756c0 .29.23.519.527.519.29 0 .527-.23.527-.52v.008Zm-2.634 0-.03-4.77a.511.511 0 1 0-1.024.007l.03 4.763a.511.511 0 0 0 1.024 0Zm-3.687-4.756.03 4.749c0 .29.23.519.512.519a.513.513 0 0 0 .511-.52l-.03-4.755c0-.29-.23-.52-.512-.52a.52.52 0 0 0-.511.527Z"
+								/>
+							</g>
+							<defs>
+								<clipPath id="a">
+									<path fill="#fff" d="M.063.93h21.375v22.138H.063z" />
+								</clipPath>
+							</defs>
+						</svg>
+					</button>
+				{/if}
+				<p>{displayedText}</p>
+			</div>
+
+			<div class="chat-bar">
+				<div class="chat-bar-wrapper">
+					<div
+						class="chat-bar-container"
+						style={`${!pointerEventsChatBar ? 'pointer-events:all' : 'pointer-events:none'}`}
+					>
+						<div class="chat-box-container">
+							<div class="chat-box-container-data">
+								<div class="chat-item">
+									<img src={user1} alt="" />
+									<div class="content-field">
+										<p>“ Hi how can i assit you today?”</p>
+									</div>
+								</div>
+								<div class="chat-item chat-item-2">
+									<img src={user2} alt="" />
+									<div class="content-field">
+										<p>“ Today is a gift that’s why it is called present”</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="chat-footer">
+							<div class="chat-footer-input">
+								<input placeholder="Chat with Aleen" type="text" />
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="27"
+									height="27"
+									fill="none"
+									viewBox="0 0 27 27"
+								>
+									<path
+										stroke="#fff"
+										stroke-linecap="square"
+										stroke-width="2.063"
+										d="m23.96 3.04-6.175 20.92h-.028l-5.897-8.82-8.82-5.896V9.21L23.924 3l.037.039ZM12.138 14.927l4.998-4.998"
+									/>
+								</svg>
+							</div>
+						</div>
+
+						<div class="chat-animate-container"></div>
+						<div class="chat-animate-container2"></div>
+					</div>
+				</div>
 			</div>
 
 			<div class="social-wrapper">
@@ -217,7 +465,7 @@
 				<a href="#" class="custom-cursor">
 					<span>Instagram</span>
 				</a>
-				<a href="#"  class="custom-cursor">
+				<a href="#" class="custom-cursor">
 					<span>Github</span>
 				</a>
 				<a href="#" class="custom-cursor">
